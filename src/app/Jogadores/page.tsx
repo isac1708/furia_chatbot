e57cfc9788095eb6988553f4f42dfc29c3872671
isac1708 <isac.figueredo@gmail.com>
2025-04-29 12:45:19 -0300

    fix: correção importação Navbar

---
+++ src/app/Jogadores/page.tsx
@@ -0,0 +1,8 @@
+export default function JogadoresPage() {
+    return (
+      <main className="p-6 text-white">
+        <h1 className="text-2xl font-bold mb-4">Jogadores da FURIA</h1>
+        <p className="text-zinc-400">Em breve você verá aqui os jogadores e suas estatísticas!</p>
+      </main>
+    );
+  }
